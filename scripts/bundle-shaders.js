d58c060644da413a44fc2dd9dcf362135f3a837e
Estefani <stef45689@gmail.com>
2021-11-13 12:34:15 -0600

    Initial

--- scripts/bundle-shaders.js
+++ scripts/bundle-shaders.js
@@ -1,4 +1,4 @@
-let fs = require('fs-extra');
+let fs = require("fs-extra");
 
 /*
 BitmapMask.frag
@@ -10,48 +10,37 @@ Multi.frag
 Multi.vert
 */
 
-let srcdir = './src/renderer/webgl/shaders/src/';
-let destdir = './src/renderer/webgl/shaders/';
-
+let srcdir = "./src/renderer/webgl/shaders/src/";
+let destdir = "./src/renderer/webgl/shaders/";
 let files = fs.readdirSync(srcdir);
 
 files.forEach(function (file) {
-
-    let shaderSource = fs.readFileSync(srcdir + file, 'utf8');
+    let shaderSource = fs.readFileSync(srcdir + file, "utf8");
     let type = file.substr(-4);
-    let shaderFilename = file.substr(0, file.lastIndexOf('.')) + '-' + type + '.js';
+    let shaderFilename =
+        file.substr(0, file.lastIndexOf(".")) + "-" + type + ".js";
 
-    let outputSource = 'module.exports = [\n';
+    let outputSource = "module.exports = [\n";
 
-    let lines = shaderSource.split('\n');
+    let lines = shaderSource.split("\n");
 
-    for (var i = 0; i < lines.length; i++)
-    {
+    for (var i = 0; i < lines.length; i++) {
         let line = lines[i].trimRight();
 
-        if (i < lines.length - 1)
-        {
+        if (i < lines.length - 1) {
             outputSource = outputSource.concat("    '" + line + "',\n");
-        }
-        else
-        {
+        } else {
             outputSource = outputSource.concat("    '" + line + "'\n");
         }
     }
 
-    outputSource = outputSource.concat('].join(\'\\n\');\n');
+    outputSource = outputSource.concat("].join('\\n');\n");
 
     fs.writeFile(destdir + shaderFilename, outputSource, function (error) {
-
-        if (error)
-        {
+        if (error) {
             throw error;
+        } else {
+            console.log("Saved", shaderFilename);
         }
-        else
-        {
-            console.log('Saved', shaderFilename);
-        }
-
     });
-
 });
